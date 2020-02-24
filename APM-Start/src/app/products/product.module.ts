4fa03cffb2d220e733de48ec5ac30b66eeab1fb2
Murray <KATE.MURRAY@VIZIENTINC.COM>
2020-02-24 16:31:15 -0600

    Separated routing into modules

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from "@angular/core";
 import { ProductListComponent } from "./product-list.component";
 import { ConvertToSpacesPipe } from "../shared/convert-to-spaces.pipe";
-import { RouterModule } from "@angular/router";
 import { ProductDetailComponent } from "./product-detail.component";
-import { ProductDetailGuard } from "./product-detail.guard";
 import { SharedModule } from "../shared/shared.module";
+import { ProductRoutingModule } from "./product-routing.module";
 
 @NgModule({
   declarations: [
@@ -12,19 +11,6 @@ import { SharedModule } from "../shared/shared.module";
     ProductDetailComponent,
     ConvertToSpacesPipe
   ],
-  imports: [
-    RouterModule.forChild([
-      {
-        path: "products",
-        component: ProductListComponent
-      },
-      {
-        path: "products/:id",
-        canActivate: [ProductDetailGuard],
-        component: ProductDetailComponent
-      }
-    ]),
-    SharedModule
-  ]
+  imports: [SharedModule, ProductRoutingModule]
 })
 export class ProductModule {}
